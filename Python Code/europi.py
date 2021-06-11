5d2c95590a9feb1cb9650243aac076a2c54337bf
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2021-06-11 08:53:23 +0100

    Fix knob mixup, Add knob return value (0-65535)

--- Python Code/europi.py
+++ Python Code/europi.py
@@ -4,9 +4,9 @@ from time import sleep
 
 
 ####PINS####
-#Don't change these values unless you are modifying the hardware, as the pins connected to the Pico are preset by the PCB layout.
-knob_2 = ADC(Pin(27))
-knob_1 = ADC(Pin(28))
+
+knob_2 = ADC(Pin(28))
+knob_1 = ADC(Pin(27))
 button_1 = Pin(15, Pin.IN, Pin.PULL_UP)
 button_2 = Pin(18, Pin.IN, Pin.PULL_UP)
 digital_1 = Pin(21, Pin.OUT)
@@ -20,22 +20,25 @@ analogue_4  = PWM(Pin(7, Pin.OUT))
 
 
 ####CLASSES####
+
 class knob:
     def __init__(self, pin):
         self.pin = pin
         
-    def percent(self):  #This allows a knob to be read as a percentage, which makes it much easier to use for random chance.
+    def percent(self):
         return int(self.pin.read_u16() / 655.35)
     
+    def value(self):
+        return(self.pin.read_u16())
 
 class analogue_pin:
     def __init__(self, pin):
         self.pin = pin
         
-    def value(self, new_duty):  #Accepts 0-65535
+    def value(self, new_duty):
         self.pin.duty_u16(new_duty)
         
-    def randomise(self):    #Assigns the value automatically to a random voltage
+    def randomise(self):
         self.duty(randint(0,65034))
         
 
@@ -43,20 +46,33 @@ class digital_pin:
     def __init__(self, pin):
         self.pin = pin
         
-    def trigger(self):  #Sends a EuroRack standard trigger. Not usable if you're triggering multiple pins simultaneously
+    def trigger(self):
         self.pin.value(1)
         sleep(0.05)
         self.pin.value(0)
         
-    def value(self, value): #Accepts 0-1, off/on
+    def value(self, value):
         self.pin.value(value)
         
-    def toggle(self):   #Toggles the value, helpful if the current value isn't known/required
+    def toggle(self):
         self.pin.toggle()
-      
-    
-####FUNCTIONS####      
-def create_scale(notes):    #Creates a chromatic scale based on the given positions of notes within a chromatic octave. Positions start at 1 for ease of use
+        
+
+####FUNCTIONS####        
+
+def strum(trigger_pin, pitch_pin, count, time, notes):
+    if len(notes) != count:
+        print("Error: Please make sure you have a note pitch per pluck")
+    else:
+        for pluck in range(0,count-1):
+            pitch_pin.value(notes[pluck])
+            trigger_pin.value(1)
+            sleep(time[0])
+            trigger_pin.value(0)
+            sleep(time[1])
+            
+            
+def create_scale(notes):
     global chromatic_step
     scale = []
     note = 0
@@ -70,7 +86,7 @@ def create_scale(notes):    #Creates a chromatic scale based on the given positi
             step = 1
     return scale
 
-def random_chance(percentage):  #Calculates a boolean output based on a given percentage chance of being True
+def random_chance(percentage):
     if randint(0,100) < percentage:
         return True
     else:
@@ -78,23 +94,19 @@ def random_chance(percentage):  #Calculates a boolean output based on a given pe
           
           
 ####VARIABLES####
-chromatic_step = 65536 / (11.75 * 3.3)  #Don't change this unless you are having 1V/Oct tracking issues. The method used for converting digital to analogue is imperfect, so the value of 11.75 is carefully calculated to account
+    
+chromatic_step = 65536 / (11.75 * 3.3)
             
-####SCALES####
-#To create a scale, simply use the create_scale function, passing in an array with the position of the notes in the scale.
-#For example, a c_maj scale is formed of the 1st, 3rd, 5th, 6th, 8th, 10th, and 12th notes of a chromatic octave. 
 c_maj = create_scale([1,3,5,6,8,10,12])
 d_maj = create_scale([3,5,7,8,10,12])
 d_min = create_scale([3,5,6,8,10,11])
 jazz = create_scale([1,4,7,8,11])
 
-#Bass scales, often useful if you want a smaller range of a scale
 d_maj_bass = d_maj[0:8]
 d_min_bass = d_min[0:8]
 c_maj_bass = c_maj[0:8]
 jazz_bass = jazz[0:8]
 
-#Re-define the pins as objects, allowing their new methods to be accessed
 analogue_1 = analogue_pin(analogue_1)
 analogue_2 = analogue_pin(analogue_2)
 analogue_3 = analogue_pin(analogue_3)
@@ -106,7 +118,6 @@ digital_4 = digital_pin(digital_4)
 knob_1 = knob(knob_1)
 knob_2 = knob(knob_2)
 
-#Ignore this section as this module is always imported rather than run itself
 if __name__ == "__main__":
     None
 else:
