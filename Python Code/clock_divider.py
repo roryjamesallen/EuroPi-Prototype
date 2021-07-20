d129174c7f3e6c15048585e77c22f6d9cf751907
Adam Wonak <adam.wonak@gmail.com>
2021-07-20 18:13:59 -0500

    division choices should be a const

--- Python Code/clock_divider.py
+++ Python Code/clock_divider.py
@@ -5,7 +5,7 @@ version: 1.0
 
 Provide 4 divisions of the master clock set by knob 1.
 
-Master clock (divide by 1) will emit a trigger pulse once every quarter note
+Master clock set by knob 1 will emit a trigger pulse once every quarter note
 for the current tempo. Use button 2 to cycle through each digital out enabling
 it to set a division from a list of division choices chosen by knob 2.
 
@@ -28,8 +28,8 @@ MIN_BPM = 20
 MAX_BPM = 280
 
 # Useful divisions to choose from.
-division_choices = [1, 2, 3, 4, 5, 6, 7, 8, 12, 16]
-MAX_DIVISION = max(division_choices)
+DIVISION_CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 12, 16]
+MAX_DIVISION = max(DIVISION_CHOICES)
 
 # Divisions corresponding to each digital output.
 divisions = [1, 2, 4, 8]
@@ -38,7 +38,8 @@ divisions = [1, 2, 4, 8]
 outputs = [digital_1, digital_2, digital_3, digital_4]
 
 selected_output = -1
-previous_choice = int(knob_2.percent() * len(division_choices) - 0.0001)
+previous_choice = int(knob_2.percent() * len(DIVISION_CHOICES) - 0.0001)
+counter = 1
 
 
 def config_divisions():
@@ -50,7 +51,6 @@ button_2.handler(config_divisions)
 
 
 # Start the main loop.
-counter = 1
 while True:
     # Set the clock speed via Knob 1.
     # tempo range will be between 20 and 280 BPM.
@@ -68,14 +68,14 @@ while True:
 
     # Set the currently selected digital out's clock division to the value
     # selected by knob 2.
-    choice = int(knob_2.percent() * len(division_choices) - 0.0001)
+    choice = int(knob_2.percent() * len(DIVISION_CHOICES) - 0.0001)
     if selected_output >= 0 and choice != previous_choice:
-        divisions[selected_output] = division_choices[choice]
+        divisions[selected_output] = DIVISION_CHOICES[choice]
         previous_choice = choice
 
         if DEBUG:
             msg = "Change DJ{} division to: {}"
-            print(msg.format(selected_output, division_choices[choice]))
+            print(msg.format(selected_output, DIVISION_CHOICES[choice]))
 
     # Wrap the counter if we've reached the largest division.
     counter = (counter + 1) % MAX_DIVISION
