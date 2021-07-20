42bd12e2ee044a0fa751d3280eb9ab2de8fd76ea
Adam Wonak <adam.wonak@gmail.com>
2021-07-19 20:43:35 -0500

    refactored clock_divider to sync triggers and allow changing divisions for each output on the fly.

--- Python Code/clock_divider.py
+++ Python Code/clock_divider.py
@@ -5,7 +5,13 @@ version: 1.0
 
 Provide 4 divisions of the master clock set by knob 1.
 
+Master clock (divide by 1) will emit a trigger pulse once every quarter note
+for the current tempo. Use button 2 to cycle through each digital out enabling
+it to set a division from a list of division choices chosen by knob 2.
+
 knob_1: master clock tempo
+knob_2: choose the division for the current selected index.
+button_2: cycle through the digital
 digital_1: first division, default master clock
 digital_2: second division, default /2
 digital_3: third division, default /4
@@ -16,12 +22,38 @@ from europi import *
 
 import time
 
+DEBUG = False
+
 MIN_BPM = 20
 MAX_BPM = 280
 
-# Useful divisions: [1, 2, 3, 4, 5, 6, 7, 8, 12, 16]
-# Length of divisions must not exceed 4 (the number of digital pins).
+# Useful divisions to choose from.
+division_choices = [1, 2, 3, 4, 5, 6, 7, 8, 12, 16]
+MAX_DIVISION = max(division_choices)
+
+# Divisions corresponding to each digital output.
 divisions = [1, 2, 4, 8]
+
+# Each digital output object.
+outputs = [digital_1, digital_2, digital_3, digital_4]
+
+config_index = -1
+
+
+def config_divisions():
+    global config_index
+    config_index = (config_index + 1) % len(divisions)
+    if DEBUG:
+        print("New division config index: ", config_index)
+button_2.handler(config_divisions)
+
+
+# Provide a list of pins to trigger in sync.
+def sync_trigger(pins):
+    [pin.value(1) for pin in pins]
+    time.sleep(0.05)
+    [pin.value(0) for pin in pins]
+
 
 # Start the main loop.
 counter = 1
@@ -30,16 +62,28 @@ while True:
     # tempo range will be between 20 and 280 BPM.
     # Knob 12 o'clock position is 150 BPM.
     tempo = (knob_1.percent() * (MAX_BPM - MIN_BPM)) + MIN_BPM
-    time.sleep(60 / tempo)
+    # Sleep for a quarter note of the tempo.
+    time.sleep((60 / tempo) / 4)
 
     # Trigger the digital pin if it's divisible by the counter.
-    for i, pin in enumerate([digital_1, digital_2, digital_3, digital_4]):
+    pins = []
+    for i, pin in enumerate(outputs):
         if counter % divisions[i] == 0:
-            pin.trigger()
+            pins.append(pin)
+    sync_trigger(pins)
+
+    # Change the current config index digital pin to a division chosen by the
+    # current knob 2 value.
+    choice = int(knob_2.percent() * len(division_choices) - 0.0001)
+    if config_index >= 0 and division_choices[choice] != divisions[config_index]:
+        divisions[config_index] = division_choices[choice]
+
+        if DEBUG:
+            print("Change DJ{} division to: {}".format(config_index, division_choices[choice]))
 
     # Wrap the counter if we've reached the largest division.
-    if counter == divisions[-1]:
-        counter = 1
-    else:
-        counter += 1
+    counter = (counter + 1) % MAX_DIVISION
+
+    if DEBUG:
+        print("DJ: {}  || config:{}  tempo:{}".format(divisions, config_index, tempo))
 
